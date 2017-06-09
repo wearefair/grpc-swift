ad8c333bd980c6312c00d0b54bf52a3cb66aeb3b
Tim Burks <timburks@google.com>
2017-06-08 21:46:17 -0700

    Expand datastore example into a very simple command-line tool.
    Currently it creates, deletes, and queries entities (very crudely).

--- Examples/Datastore/PackageManager/Sources/main.swift
+++ Examples/Datastore/PackageManager/Sources/main.swift
@@ -20,51 +20,114 @@ import OAuth2
 let CREDENTIALS = "google.yaml" // in $HOME/.credentials
 let TOKEN = "google.json" // local auth token storage
 
-#if os(OSX)
-// On OS X, we use the local browser to help the user get a token.
-let tokenProvider = try BrowserTokenProvider(credentials:CREDENTIALS, token:TOKEN)
-if tokenProvider.token == nil {
-  try tokenProvider.signIn(scopes:["profile",
-                                   "https://www.googleapis.com/auth/contacts.readonly",
-                                   "https://www.googleapis.com/auth/cloud-platform"])
-  try tokenProvider.saveToken(TOKEN)
+func main () throws {
+  let arguments = CommandLine.arguments
+
+  if arguments.count == 1 {
+    print("Usage: \(arguments[0]) [options]")
+    return
+  }
+
+  #if os(OSX)
+    // On OS X, we use the local browser to help the user get a token.
+    let tokenProvider = try BrowserTokenProvider(credentials:CREDENTIALS, token:TOKEN)
+    if tokenProvider.token == nil {
+      try tokenProvider.signIn(scopes:["profile",
+                                       "https://www.googleapis.com/auth/contacts.readonly",
+                                       "https://www.googleapis.com/auth/cloud-platform"])
+      try tokenProvider.saveToken(TOKEN)
+    }
+  #else
+    // On Linux, we can get a token if we are running in Google Cloud Shell
+    // or in some other Google Cloud instance (GAE, GKE, GCE, etc).
+    let tokenProvider = try GoogleTokenProvider()
+  #endif
+
+  gRPC.initialize()
+
+  guard let authToken = tokenProvider.token?.accessToken else {
+    print("ERROR: No OAuth token is avaiable.")
+    exit(-1)
+  }
+
+  let projectID = "hello-86"
+  let certificateURL = URL(fileURLWithPath:"roots.pem")
+  let certificates = try! String(contentsOf: certificateURL, encoding: .utf8)
+  let service = Google_Datastore_V1_DatastoreService(address:"datastore.googleapis.com",
+                                                     certificates:certificates,
+                                                     host:nil)
+
+  service.metadata = Metadata(["authorization":"Bearer " + authToken])
+
+  switch arguments[1] {
+
+  case "query":
+    var queryRequest = Google_Datastore_V1_RunQueryRequest()
+    queryRequest.projectID = projectID
+    var query = Google_Datastore_V1_GqlQuery()
+    query.queryString = "select *"
+    queryRequest.gqlQuery = query
+    print("\(queryRequest)")
+    let queryResult = try service.runquery(queryRequest)
+    print("\(queryResult)")
+
+  case "create":
+    var beginTransactionRequest = Google_Datastore_V1_BeginTransactionRequest()
+    beginTransactionRequest.projectID = projectID
+    let result = try service.begintransaction(beginTransactionRequest)
+    print("\(result)")
+
+    var commitRequest = Google_Datastore_V1_CommitRequest()
+    commitRequest.projectID = projectID
+    commitRequest.transaction = result.transaction
+
+    var nameValue = Google_Datastore_V1_Value()
+    nameValue.stringValue = "Anonymous Person"
+    var entity = Google_Datastore_V1_Entity()
+    entity.properties["name"] = nameValue
+    var key = Google_Datastore_V1_Key()
+    var pathElement = Google_Datastore_V1_Key.PathElement()
+    pathElement.kind = "Person"
+    key.path.append(pathElement)
+    entity.key = key
+
+    var mutation = Google_Datastore_V1_Mutation()
+    mutation.insert = entity
+
+    commitRequest.mutations.append(mutation)
+    let commitResult = try service.commit(commitRequest)
+    print("\(commitResult)")
+
+  case "delete":
+    var beginTransactionRequest = Google_Datastore_V1_BeginTransactionRequest()
+    beginTransactionRequest.projectID = projectID
+    let result = try service.begintransaction(beginTransactionRequest)
+    print("\(result)")
+
+    var deleteRequest = Google_Datastore_V1_CommitRequest()
+    deleteRequest.projectID = projectID
+    deleteRequest.transaction = result.transaction
+
+    var key = Google_Datastore_V1_Key()
+    var pathElement = Google_Datastore_V1_Key.PathElement()
+    pathElement.kind = "Person"
+    pathElement.id = Int64(arguments[2])!
+    key.path.append(pathElement)
+
+    var mutation = Google_Datastore_V1_Mutation()
+    mutation.delete = key
+
+    deleteRequest.mutations.append(mutation)
+    let deleteResult = try service.commit(deleteRequest)
+    print("\(deleteResult)")
+
+  default:
+    print("unknown command: \(arguments)")
+  }
 }
-#else
-// On Linux, we can get a token if we are running in Google Cloud Shell
-// or in some other Google Cloud instance (GAE, GKE, GCE, etc).
-let tokenProvider = try GoogleTokenProvider()
-#endif
-
-gRPC.initialize()
-
-guard let authToken = tokenProvider.token?.accessToken else {
-  print("ERROR: No OAuth token is avaiable.")
-  exit(-1)
-}
-
-let projectID = "YOUR PROJECT ID"
-
-let certificateURL = URL(fileURLWithPath:"roots.pem")
-let certificates = try! String(contentsOf: certificateURL, encoding: .utf8)
-let service = Google_Datastore_V1_DatastoreService(address:"datastore.googleapis.com",
-                                                   certificates:certificates,
-                                                   host:nil)
-
-service.metadata = Metadata(["authorization":"Bearer " + authToken])
-
-var request = Google_Datastore_V1_RunQueryRequest()
-request.projectID = projectID
-
-var query = Google_Datastore_V1_GqlQuery()
-query.queryString = "select *"
-
-request.gqlQuery = query
-
-print("\(request)")
 
 do {
-  let result = try service.runquery(request)
-  print("\(result)")
+  try main()
 } catch (let error) {
   print("ERROR: \(error)")
 }
